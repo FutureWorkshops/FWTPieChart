ee3b59653058407a580d0e8335227bf11476e961
Carlos <carlos@futureworkshops.com>
2014-04-05 17:38:06 +0100

    created info view controller

---
+++ FWTPieChartExample/FWTPieChartExample/UI/FWTInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  FWTInfoViewController.h
+//  FWTPieChartExample
+//
+//  Created by Carlos Vidal on 05/04/2014.
+//  Copyright (c) 2014 Carlos Vidal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FWTInfoViewController : UIViewController
+
+@end
---
+++ FWTPieChartExample/FWTPieChartExample/UI/FWTInfoViewController.m
@@ -0,0 +1,49 @@
+//
+//  FWTInfoViewController.m
+//  FWTPieChartExample
+//
+//  Created by Carlos Vidal on 05/04/2014.
+//  Copyright (c) 2014 Carlos Vidal. All rights reserved.
+//
+
+#import "FWTInfoViewController.h"
+
+@interface FWTInfoViewController ()
+
+@end
+
+@implementation FWTInfoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

67cc77cc7d6864472b0353495a8681f68f04a74d
Carlos <carlos@futureworkshops.com>
2014-04-05 17:51:28 +0100

    created base navigation controller

---
+++ FWTPieChartExample/FWTPieChartExample/UI/FWTBaseNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  FWTBaseNavigationController.h
+//  FWTPieChartExample
+//
+//  Created by Carlos Vidal on 05/04/2014.
+//  Copyright (c) 2014 Carlos Vidal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FWTBaseNavigationController : UINavigationController
+
+@end

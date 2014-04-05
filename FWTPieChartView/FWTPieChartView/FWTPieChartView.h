b66a33a0827b6df2362d1ea51504299ca702db7e
Carlos <carlos@futureworkshops.com>
2014-04-05 13:23:54 +0100

    copied pieChartView code to library

--- FWTPieChartView/FWTPieChartView/FWTPieChartView.h
+++ FWTPieChartView/FWTPieChartView/FWTPieChartView.h
@@ -6,8 +6,39 @@
 //  Copyright (c) 2014 Carlos Vidal. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface FWTPieChartView : NSObject
+//FWTPieChartSegmentData
+@interface FWTPieChartSegmentData : NSObject
+
+@property (nonatomic, strong) NSNumber *value;
+@property (nonatomic, strong) UIColor *color;
+@property (nonatomic, strong) NSString *innerText;
+@property (nonatomic, strong) NSString *outerText;
+
++ (FWTPieChartSegmentData*)pieChartSegmentWithValue:(NSNumber*)value
+                                              color:(UIColor*)color
+                                          innerText:(NSString*)innerText
+                                       andOuterText:(NSString*)outerText;
+
+@end
+
+
+//FWTPieChartView
+@interface FWTPieChartView : UIView
+
+@property (nonatomic, strong) NSArray *segments;
+
+@property (nonatomic, strong) UIFont *font;
+@property (nonatomic, assign) BOOL shouldDrawSeparators;
+@property (nonatomic, assign) BOOL shouldDrawPercentages;
+@property (nonatomic, assign) float innerCircleProportionalRadius;
+
+- (void)addSegment:(FWTPieChartSegmentData*)segmentData;
+- (FWTPieChartSegmentData*)addSegmentWithValue:(NSNumber*)value color:(UIColor*)color innerText:(NSString*)innerText andouterText:(NSString*)outerText;
+- (void)removeSegment:(FWTPieChartSegmentData*)segmentData;
+- (void)clearSegments;
+
+- (void)reloadAnimated:(BOOL)animated;
 
 @end

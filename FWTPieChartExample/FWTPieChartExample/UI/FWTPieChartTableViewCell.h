76b3f5dd2f0d60bafc90e4e189fb46f173e3232d
Carlos <carlos@futureworkshops.com>
2014-04-05 12:49:39 +0100

    created pieChart cell

---
+++ FWTPieChartExample/FWTPieChartExample/UI/FWTPieChartTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FWTPieChartTableViewCell.h
+//  FWTPieChartExample
+//
+//  Created by Carlos Vidal on 05/04/2014.
+//  Copyright (c) 2014 Carlos Vidal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FWTPieChartTableViewCell : UITableViewCell
+
+@end
---
+++ FWTPieChartExample/FWTPieChartExample/UI/FWTPieChartTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  FWTPieChartTableViewCell.m
+//  FWTPieChartExample
+//
+//  Created by Carlos Vidal on 05/04/2014.
+//  Copyright (c) 2014 Carlos Vidal. All rights reserved.
+//
+
+#import "FWTPieChartTableViewCell.h"
+
+@implementation FWTPieChartTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
